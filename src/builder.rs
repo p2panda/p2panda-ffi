add1f672125d1dde5f3a5041ffc4a0fd9635e0a3
adz <x12@adz.garden>
2026-05-18 19:19:00 +0200

    WIP: Add doc-strings

--- src/builder.rs
+++ src/builder.rs
@@ -8,9 +8,20 @@ use thiserror::Error;
 use crate::core::{NetworkId, RelayUrl, SigningKey, VerifyingKey};
 use crate::node::{Node, SpawnError};
 
+/// Message acknowledgement policy for eventually-consistent topic streams.
+///
+/// Every `StreamSubscription` instance is stateful and keeps track of already acknowledged
+/// operations by persisting them in the local SQLite database. Specifying a policy defines how and
+/// when events are acknowledged.
+///
+/// Operations which have not been acknowledged yet will be automatically re-played when this stream
+/// is created again.
 #[derive(uniffi::Enum)]
 pub enum AckPolicy {
+    /// Each individual message must be acknowledged.
     Explicit,
+
+    /// No manual acknowledgment needed, node assumes acknowledgment on delivery.
     Automatic,
 }
 
@@ -23,19 +34,28 @@ impl From<AckPolicy> for p2panda::node::AckPolicy {
     }
 }
 
+/// mDNS discovery mode.
+///
+/// By default this is set to "active" meaning we are actively advertising our address and public
+/// key on local-area networks.
 #[derive(uniffi::Enum)]
 pub enum MdnsDiscoveryMode {
+    /// mDNS discovery disabled.
     Disabled,
-    Passive,
+
+    /// Advertise our own and listen for others' discovery announcements.
     Active,
+
+    /// Listen for others' discovery announcements but don't advertise our own.
+    Passive,
 }
 
 impl From<MdnsDiscoveryMode> for p2panda::network::MdnsDiscoveryMode {
     fn from(value: MdnsDiscoveryMode) -> Self {
         match value {
             MdnsDiscoveryMode::Disabled => Self::Disabled,
-            MdnsDiscoveryMode::Passive => Self::Passive,
             MdnsDiscoveryMode::Active => Self::Active,
+            MdnsDiscoveryMode::Passive => Self::Passive,
         }
     }
 }
@@ -60,6 +80,7 @@ pub enum NodeBuilderError {
     IpAddr(#[from] IpAddrError),
 }
 
+/// Builder for `Node`.
 #[derive(uniffi::Object)]
 pub struct NodeBuilder(Mutex<Option<p2panda::NodeBuilder>>);
 
@@ -83,6 +104,7 @@ impl NodeBuilder {
 
 #[uniffi::export]
 impl NodeBuilder {
+    /// Returns the builder for a `Node`.
     #[uniffi::constructor]
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
@@ -90,26 +112,94 @@ impl NodeBuilder {
         Self(Mutex::new(Some(inner)))
     }
 
+    /// Sets the signing key.
+    ///
+    /// The public key derived from the given private key is used to identify the node in the
+    /// network. For example, this key can be used to directly connect to the node. The private key
+    /// serves as the means of authenticating the node during the connection handshake (using TLS
+    /// 1.3) and is also used to sign operations to ensure data integrity and authenticity.
+    ///
+    /// If left unset, a new key will be randomly generated.
     pub fn signing_key(&self, signing_key: Arc<SigningKey>) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.signing_key(signing_key.to_inner()))
     }
 
+    /// Defines the database URL to be used by the store.
+    ///
+    /// The given URL must take the form of a SQLite database URI: <https://sqlite.org/uri.html>.
+    ///
+    /// Database migrations are run automatically. Users should use `NodeBuilder::database_pool()`
+    /// together with `sqlx` if manual migration management is required.
+    ///
+    /// If left unset, the node will default to using an ephemeral in-memory database.
     pub fn database_url(&self, url: &str) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.database_url(url))
     }
 
+    /// Defines the acknowledgement policy.
+    ///
+    /// If left unset, the policy defaults to `Automatic` and all messages emitted from topic
+    /// streams will be automatically acknowledged.
+    ///
+    /// See the `Node::stream(topic)` documentation for further information.
     pub fn ack_policy(&self, ack_policy: AckPolicy) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.ack_policy(ack_policy.into()))
     }
 
+    /// Sets the network identifier.
+    ///
+    /// The network identifier is used to achieve separation and prevent interoperability between
+    /// distinct networks. This is the most global identifier to group nodes into networks. Different
+    /// applications may choose to share the same underlying network infrastructure by using the same
+    /// network identifier.
+    ///
+    /// **WARNING:** The network identifier is _not_ confidentially exchanged with a remote node and
+    /// can not be treated as a secret value. See: <https://github.com/p2panda/p2panda/issues/965>
+    ///
+    /// If left unset, the network ID defaults to the byte representation of the BLAKE3 hash of the
+    /// string "p2panda".
     pub fn network_id(&self, network_id: Arc<NetworkId>) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.network_id(network_id.to_inner()))
     }
 
+    /// Sets a relay server URL to assist in establishing direct connections.
+    ///
+    /// Multipe relays can be added; a single "home relay" will be automatically selected based on
+    /// latency.
+    ///
+    /// Relays fullfil multiple functions:
+    ///
+    /// 1. The relay server helps establish connections by temporarily routing encrypted traffic
+    ///    until a direct, P2P connection is feasible. This allows nodes to immediately get
+    ///    started, without waiting for holepunching / STUN to complete first.
+    /// 2. Handle learning a node's public addresses (via QUIC address discovery), signalling and
+    ///    hole-punching to establish direct connections between two nodes. This set of methods is
+    ///    also understood as STUN. After this point the relay is not required anymore.
+    /// 3. Relayed and encrypted fallback using the server when establishing a direct connection
+    ///    failed (TURN).
+    ///
+    /// If no relay is given other nodes can only connect to us if a directly-reachable IP address
+    /// is available and known to them.
     pub fn relay_url(&self, url: Arc<RelayUrl>) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.relay_url(url.to_inner()))
     }
 
+    /// Inserts a bootstrap node into the local address book.
+    ///
+    /// Bootstrap nodes are used as a starting point for the random-walk discovery algorithm to
+    /// find other nodes in the network, without the need for any centralised registry. Any node
+    /// can serve as a bootstrap into the network. The URL of the relay used by the bootstrap node
+    /// is required to assist with connectivity (via relaying of traffic and negotiation of
+    /// hole-punching for direct connections).
+    ///
+    /// Multiple bootstrap nodes can be registered. Each iteration of the discovery algorithm
+    /// begins by picking a random node from the set of known bootstrap nodes. It's recommended to
+    /// register several bootstrap nodes, especially if they are not highly-available; this
+    /// offers redunancy in the case that any of the bootstrap nodes go offline or are otherwise
+    /// unavailable.
+    ///
+    /// Consult the documentation of the `p2panda-discovery` crate for further details concerning
+    /// the discovery protocol.
     pub fn bootstrap(
         &self,
         node_id: Arc<VerifyingKey>,
@@ -118,26 +208,46 @@ impl NodeBuilder {
         self.update(|builder| builder.bootstrap(node_id.to_inner(), relay_url.to_inner()))
     }
 
+    /// Sets the mDNS discovery mode.
+    ///
+    /// mDNS may be set to active, passive or disabled mode.
+    ///
+    /// If left unset, the mode defaults to active and this node will actively advertise it's
+    /// endpoint address on the local area network.
     pub fn mdns_mode(&self, mode: MdnsDiscoveryMode) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.mdns_mode(mode.into()))
     }
 
+    /// Binds an IPv4 socket at the given address.
+    ///
+    /// If left unset, the address defaults to `0.0.0.0`.
     pub fn bind_ip_v4(&self, ip_address: &str) -> Result<(), NodeBuilderError> {
         let ip_address = std::net::Ipv4Addr::from_str(ip_address)
             .map_err(|err| IpAddrError::ParseInvalidAddr(err.to_string()))?;
         self.update(|builder| builder.bind_ip_v4(ip_address))
     }
 
+    /// Sets the IPv4 address port.
+    ///
+    /// If left unset, the port defaults to `0` which results in a random free port being chosen.
+    /// If the given port is already in use, a random port will be chosen as a fallback.
     pub fn bind_port_v4(&self, port: u16) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.bind_port_v4(port))
     }
 
+    /// Binds an IPv6 socket at the given address.
+    ///
+    /// If left unset, the address defaults to `[::]`.
     pub fn bind_ip_v6(&self, ip_address: &str) -> Result<(), NodeBuilderError> {
         let ip_address = std::net::Ipv6Addr::from_str(ip_address)
             .map_err(|err| IpAddrError::ParseInvalidAddr(err.to_string()))?;
         self.update(|builder| builder.bind_ip_v6(ip_address))
     }
 
+    /// Sets the IPv6 address port.
+    ///
+    /// If left unset, the port defaults to `0` which results in a random free port being chosen.
+    /// If the given port is already in use, a random port will be chosen as a fallback.
     pub fn bind_port_v6(&self, port: u16) -> Result<(), NodeBuilderError> {
         self.update(|builder| builder.bind_port_v6(port))
     }
@@ -145,6 +255,7 @@ impl NodeBuilder {
 
 #[uniffi::export(async_runtime = "tokio")]
 impl NodeBuilder {
+    /// Spawns the `Node`.
     pub async fn spawn(&self) -> Result<Node, SpawnError> {
         let builder = self.take()?;
         let inner = builder.spawn().await?;
--- src/core.rs
+++ src/core.rs
@@ -103,6 +103,20 @@ impl From<p2panda_core::identity::IdentityError> for ConversionError {
     }
 }
 
+/// Identifier for a network.
+///
+/// The network identifier is used to achieve separation and prevent interoperability between
+/// distinct networks. This is the most global identifier to group nodes into networks. Different
+/// applications may choose to share the same underlying network infrastructure by using the same
+/// network identifier.
+///
+/// A BLAKE3 hash function is performed against each protocol identifier which is registered with
+/// `p2panda-net`, using the network identifier as an additional input. Even if two instances of
+/// `p2panda-net` are created with the same network protocols, any communication attempts will fail
+/// if they are not using the same network identifier.
+///
+/// **WARNING:** The network identifier is _not_ confidentially exchanged with a remote node and
+/// can not be treated as a secret value. See: <https://github.com/p2panda/p2panda/issues/965>
 #[derive(uniffi::Object)]
 pub struct NetworkId(ByteString);
 
@@ -114,30 +128,40 @@ impl NetworkId {
 
 #[uniffi::export]
 impl NetworkId {
+    /// Generate new, random network identifier.
     #[uniffi::constructor]
     pub fn random() -> Self {
         Self(ByteString::random())
     }
 
+    /// Creates network identifier from raw bytes.
+    ///
+    /// Needs to be exactly 32 bytes, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_bytes(value: &[u8]) -> Result<Self, ConversionError> {
         Ok(Self(ByteString::from_bytes(value)?))
     }
 
+    /// Creates network identifier from hash.
     #[uniffi::constructor]
     pub fn from_hash(hash: Arc<Hash>) -> Self {
         Self(ByteString::from_hash(hash))
     }
 
+    /// Creates network identifier from hexadecimal string.
+    ///
+    /// Needs to be exactly 64 valid, hexadecimal characters, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_hex(value: &str) -> Result<Self, ConversionError> {
         Ok(Self(ByteString::from_hex(value)?))
     }
 
+    /// Returns network id represented in bytes.
     pub fn to_bytes(&self) -> Vec<u8> {
         self.0.to_bytes()
     }
 
+    /// Returns network id represented as a hexadecimal string.
     pub fn to_hex(&self) -> String {
         self.0.to_hex()
     }
@@ -149,6 +173,17 @@ impl From<p2panda::NetworkId> for NetworkId {
     }
 }
 
+/// Identifier for a gossip- or sync topic.
+///
+/// A topic identifier is required when subscribing or publishing to a stream.
+///
+/// Topics usually describe concrete data which nodes want to exchange over, for example a document
+/// id or chat group id and so forth. Applications usually want to share topics via a secure side
+/// channel.
+///
+/// **WARNING:** Sensitive topics have to be treated like secret values and generated using a
+/// cryptographically secure pseudorandom number generator (CSPRNG). Otherwise they can be easily
+/// guessed by third parties or leaked during discovery.
 #[derive(uniffi::Object)]
 pub struct Topic(ByteString);
 
@@ -160,30 +195,40 @@ impl Topic {
 
 #[uniffi::export]
 impl Topic {
+    /// Generate new, random topic.
     #[uniffi::constructor]
     pub fn random() -> Self {
         Self(ByteString::random())
     }
 
+    /// Creates topic from raw bytes.
+    ///
+    /// Needs to be exactly 32 bytes, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_bytes(value: &[u8]) -> Result<Self, ConversionError> {
         Ok(Self(ByteString::from_bytes(value)?))
     }
 
+    /// Creates topic from hash.
     #[uniffi::constructor]
     pub fn from_hash(hash: Arc<Hash>) -> Self {
         Self(ByteString::from_hash(hash))
     }
 
+    /// Creates topic from hexadecimal string.
+    ///
+    /// Needs to be exactly 64 valid, hexadecimal characters, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_hex(value: &str) -> Result<Self, ConversionError> {
         Ok(Self(ByteString::from_hex(value)?))
     }
 
+    /// Returns topic represented in bytes.
     pub fn to_bytes(&self) -> Vec<u8> {
         self.0.to_bytes()
     }
 
+    /// Returns topic represented as a hexadecimal string.
     pub fn to_hex(&self) -> String {
         self.0.to_hex()
     }
@@ -195,6 +240,7 @@ impl From<p2panda_core::Topic> for Topic {
     }
 }
 
+/// Public Ed25519 key used for identifying peers and verifying signed data.
 #[derive(Debug, uniffi::Object)]
 pub struct VerifyingKey(p2panda_core::VerifyingKey);
 
@@ -206,24 +252,33 @@ impl VerifyingKey {
 
 #[uniffi::export]
 impl VerifyingKey {
+    /// Creates verifying key from raw bytes.
+    ///
+    /// Needs to be exactly 32 bytes, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_bytes(value: &[u8]) -> Result<Self, ConversionError> {
         Ok(Self(p2panda_core::VerifyingKey::try_from(value)?))
     }
 
+    /// Creates verifying key from hexadecimal string.
+    ///
+    /// Needs to be exactly 64 valid, hexadecimal characters, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_hex(value: &str) -> Result<Self, ConversionError> {
         Ok(Self(p2panda_core::VerifyingKey::from_str(value)?))
     }
 
+    /// Returns verifying key represented in bytes.
     pub fn to_bytes(&self) -> Vec<u8> {
         self.0.as_bytes().to_vec()
     }
 
+    /// Returns verifying key represented as a hexadecimal string.
     pub fn to_hex(&self) -> String {
         self.0.to_hex()
     }
 
+    /// Verify a signature over a byte slice with this public key.
     pub fn verify(&self, bytes: &[u8], signature: Arc<Signature>) -> bool {
         self.0.verify(bytes, &signature.0)
     }
@@ -235,6 +290,7 @@ impl From<p2panda_core::VerifyingKey> for VerifyingKey {
     }
 }
 
+/// Private Ed25519 key used for digital signatures.
 #[derive(uniffi::Object)]
 pub struct SigningKey(p2panda_core::SigningKey);
 
@@ -246,39 +302,52 @@ impl SigningKey {
 
 #[uniffi::export]
 impl SigningKey {
+    /// Generates a new signing key using the system's random number generator (CSPRNG) as a seed.
     #[uniffi::constructor]
     pub fn generate() -> Self {
         Self(p2panda_core::SigningKey::generate())
     }
 
+    /// Creates signing key from raw bytes.
+    ///
+    /// Needs to be exactly 32 bytes and valid Ed25519 signing key, otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_bytes(value: &[u8]) -> Result<Self, ConversionError> {
         Ok(Self(p2panda_core::SigningKey::try_from(value)?))
     }
 
+    /// Creates signing key from hexadecimal string.
+    ///
+    /// Needs to be exactly 64 valid, hexadecimal characters and valid Ed25519 signing key,
+    /// otherwise conversion will fail.
     #[uniffi::constructor]
     pub fn from_hex(value: &str) -> Result<Self, ConversionError> {
         let bytes = ByteString::from_hex(value)?.to_bytes();
         Ok(Self(p2panda_core::SigningKey::try_from(&bytes[..])?))
     }
 
+    /// Returns signing key represented in bytes.
     pub fn to_bytes(&self) -> Vec<u8> {
         self.0.as_bytes().to_vec()
     }
 
+    /// Returns signing key represented in a hexadecimal string.
     pub fn to_hex(&self) -> String {
         self.0.to_hex()
     }
 
+    /// Returns public key using this signing counterpart.
     pub fn verifying_key(&self) -> VerifyingKey {
         VerifyingKey(self.0.verifying_key())
     }
 
+    /// Sign the provided bytestring using this signing key returning a digital signature.
     pub fn sign(&self, bytes: &[u8]) -> Signature {
         Signature(self.0.sign(bytes))
     }
 }
 
+/// Ed25519 signature.
 #[derive(uniffi::Object)]
 pub struct Signature(p2panda_core::Signature);
 
@@ -304,6 +373,7 @@ impl Signature {
     }
 }
 
+/// 32-byte BLAKE3 hash.
 #[derive(Debug, uniffi::Object)]
 pub struct Hash(p2panda_core::Hash);
 
@@ -315,6 +385,7 @@ impl Hash {
 
 #[uniffi::export]
 impl Hash {
+    /// Calculate the hash of the provided bytes.
     #[uniffi::constructor]
     pub fn digest(value: &[u8]) -> Self {
         Self(p2panda_core::Hash::digest(value))
@@ -345,6 +416,7 @@ impl From<p2panda_core::Hash> for Hash {
     }
 }
 
+/// A URL identifying an iroh relay server.
 #[derive(uniffi::Object)]
 pub struct RelayUrl(p2panda::RelayUrl);
 
@@ -376,37 +448,51 @@ pub enum RelayUrlParseError {
     Invalid(String),
 }
 
+/// Header of a p2panda operation.
 #[derive(uniffi::Object)]
 pub struct Header(p2panda::operation::Header);
 
 #[uniffi::export]
 impl Header {
+    /// Operation format version, allowing backwards compatibility when specification changes.
     pub fn version(&self) -> u64 {
         self.0.version
     }
 
+    /// BLAKE3 hash of the header bytes.
+    ///
+    /// This hash is used as the unique identifier of an operation, aka the Operation Id.
     pub fn hash(&self) -> Arc<Hash> {
         Arc::new(self.0.hash().into())
     }
 
+    /// Author of this operation.
     pub fn verifying_key(&self) -> Arc<VerifyingKey> {
         Arc::new(self.0.verifying_key.into())
     }
 
+    /// Signature by author over all fields in header, providing authenticity.
     pub fn signature(&self) -> Arc<Signature> {
         Arc::new(Signature(
             self.0.signature.expect("signature always exists"),
         ))
     }
 
+    /// Time in microseconds since the Unix epoch.
     pub fn timestamp(&self) -> u64 {
         self.0.timestamp.into()
     }
 
+    /// Number of bytes of the body of this operation, must be zero if no body is given.
     pub fn payload_size(&self) -> u64 {
         self.0.payload_size
     }
 
+    /// Hash of the body of this operation, must be included if payload_size is non-zero and
+    /// omitted otherwise.
+    ///
+    /// Keeping the hash here allows us to delete the payload (off-chain data) while retaining the
+    /// ability to check the signature of the header.
     pub fn payload_hash(&self) -> Arc<Hash> {
         Arc::new(
             self.0
@@ -416,10 +502,14 @@ impl Header {
         )
     }
 
+    /// Number of operations this author has published to this log, begins with 0 and is always
+    /// incremented by 1 with each new operation by the same author.
     pub fn seq_num(&self) -> u64 {
         self.0.seq_num
     }
 
+    /// Hash of the previous operation of the same author and log. Can be omitted if first
+    /// operation in log.
     pub fn backlink(&self) -> Option<Arc<Hash>> {
         self.0.backlink.map(|hash| Arc::new(hash.into()))
     }
@@ -440,6 +530,11 @@ impl From<&p2panda::operation::Header> for Header {
     }
 }
 
+/// Cursor to track log heights (state vector).
+///
+/// It offers methods to "advance" a log and compute the difference to another state vector. A
+/// cursor can be used to manage a state vector over a topic ("log heights" of logs scoped by a
+/// topic).
 #[derive(uniffi::Object)]
 pub struct Cursor(
     p2panda_core::cursor::Cursor<p2panda_core::identity::VerifyingKey, p2panda::operation::LogId>,
